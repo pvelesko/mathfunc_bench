1c8cb43f17236f3aac72dbae27220a33f232f313
Paulius Velesko <paulius.velesko@intel.com>
2020-03-10 10:41:01 +0000

    wrap up sycl init

--- Util.hpp
+++ Util.hpp
@@ -1,4 +1,18 @@
+using namespace cl::sycl;
 int n;
+queue q;
+device dev;
+context ctx;
+
+void init() {
+  queue q(gpu_selector{});
+  device dev = q.get_device();
+  context ctx = q.get_context();
+  std::cout << "Running on "
+            << dev.get_info<info::device::name>()
+            << std::endl;
+};
+
 inline void process_args(int argc, char** argv) {
   if (argc > 1) {
     int n = std::atoi(argv[1]);
@@ -6,5 +20,11 @@ inline void process_args(int argc, char** argv) {
     n = 3;
   }
   std::cout << "Using N = " << n << std::endl;
+}
+
+template<class T>
+inline void dump(T* var, std::string name) {
+  for(int i = 0; i < n; i++)
+    std::cout << name << "[" << i << "] = " << var[i] << std::endl;
 }
 
--- main.cpp
+++ main.cpp
@@ -5,21 +5,15 @@
 using namespace cl::sycl;
 int main(int argc, char** argv) {
   process_args(argc, argv);
+  init();
   int num[3] = {1, 1, 0};
-
-  queue q(gpu_selector{});
-  device dev = q.get_device();
-  context ctx = q.get_context();
-  std::cout << "Running on "
-            << dev.get_info<info::device::name>()
-            << std::endl;
 
   { // buffer scope
     buffer<int, 1> num_d(num, range<1>(3));
     q.submit([&] (handler& cgh) { // q scope
       auto num = num_d.get_access<access::mode::read_write>(cgh);
       cgh.single_task<class oneplusone>([=] {
-        num[2] = num[0] + num[1];
+        num[2] = num[0] + cl::sycl::ceil(3.2);
       }); // end task scope
     }); // end q scope
   } // end buffer scope
